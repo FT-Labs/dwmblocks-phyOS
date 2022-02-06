4dafa43023b78bcbf9acdd5e1d0311b78224c4cf
ardaPhysTech <phystecharda@gmail.com>
2022-02-07 01:38:16 +0300

    v1.0

--- config.h
+++ config.h
@@ -1,9 +1,8 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	{"",	"crypto",	500,	6},
+	{"",	"crypto",	360,	6},
 	{"",	"weather",	18000,	5},
-	//{"",	"mailbox",	180,	12},
 	{"",	"volume",	0,	10},
 	{"",	"battery",	5,	3},
 	{"",	"clock",	60,	1},
@@ -11,5 +10,4 @@ static const Block blocks[] = {
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-//static char *delim = "┃";
-static char *delim = "  |  ";
+static char *delim = "|";
